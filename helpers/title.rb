541be1f237e9d1b3a2473c99429293dab09ecb86
Andrew Neely <aneely@gmail.com>
2020-01-30 16:19:43 -0600

    adds more comments to the title.rb file to improve readability

--- helpers/title.rb
+++ helpers/title.rb
@@ -2,13 +2,30 @@
 # Ignore this file for now; we'll come back to it.
 # It's being used by the rest of the demos, so you don't have to worry about running this one.
 
-# TL;DR: title() is a tool to make this program's output
-# a little easier to follow in the terminal. :)
-#
 # Don't worry, you'll be able to read this when we
 # come back to it at the end of the presentation! :D
+
+# You're back! You know that little `require_relative 'helpers/title'` at the top
+# of the other files? It pulls this file into that file before Ruby runs it.
+#
+# TL;DR: title() is a tool to make this program's output a little easier to
+# follow in the terminal. :) It works like you've seen in the course.
+#
+# title("Instructions!")
+# is output as
+# <Instructions!----------------------------------->
+#
+# Here's the arguments:
+#
+# text                  - a String or object that can receive a `to_s` message
+# max_length (optional) - an Integer for how much to pad the title with dashes
+#                         it's optional because we set it to 48 if there's no
+#                         second arg passed when we call it, e.g. `title("Foo")`
+#
+# returns a String
 def title(text, max_length = 48)
   padding = max_length - text.length
+
   puts "\n<#{text}#{'-' * padding}>\n\n"
 rescue ArgumentError => error
   custom_message =
@@ -17,6 +34,7 @@ rescue ArgumentError => error
       else
         ""
       end
+
   raise "#{error.class}: #{error}#{custom_message}"
 end
 
